5561e00105be819f6fa470841dfe64453e474c15
Eduardo Schwantz <eduardo_schwantz@hotmail.com>
2022-08-07 11:55:41 -0300

    Subida dos primeiros exercicios de java da lista 1

---
+++ OOP/NetBeansProjects/Lista1_ex3_java/src/lista1_ex3_java/Lista1_ex3_java.java
@@ -0,0 +1,28 @@
+package lista1_ex3_java;
+
+import java.util.Scanner;
+
+public class Lista1_ex3_java {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a, b, c, delta;
+
+        System.out.println("Programa com proposta para calcular o valor de delta.");
+
+        System.out.println("Informe o valor de a: ");
+        a = input.nextInt();
+
+        System.out.println("Informe o valor de b: ");
+        b = input.nextInt();
+
+        System.out.println("Informe o valor de c: ");
+        c = input.nextInt();
+
+        delta = (int) (Math.pow(b, 2) - (4 * a * c));
+        
+        System.out.println("O valor de delta é: "+ delta + "");
+    }
+
+}
