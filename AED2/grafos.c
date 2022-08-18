04643b4ef768cb34091b3e867342f9664d9e72e9
Eduardo Schwantz <eduardo_schwantz@hotmail.com>
2022-08-17 22:00:19 -0300

    commit

---
+++ AED2/grafos.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleOutputCP(65001);
+    system("clear||cls");
+    printf("Exercício 1 sobre grafos!!\n");
+    printf("Eduardo Schwantz.\nCecilia Botelho.\n");
+    int i, j, tam, repeat = 1;
+    do
+    {
+        printf("Digite a quantidade de vertices que deseja alocar (máximo 20!): ");
+        scanf("%d", &tam);
+
+        if (tam > 20 || tam < 0)
+        {
+            printf("\nError!\nLimite de vertices excedido!!");
+            printf("\nDigite novamente!!");
+        }
+        else
+        {
+            int matriz[tam][tam];
+            for (i = 0; i < tam; i++)
+            {
+                for (j = 0; j < tam; j++)
+                {
+                    printf("\nDigite o peso do vértice linha %d coluna %d: ", i+1, j+1);
+                    scanf("%d", &matriz[i][j]);
+                }
+            }
+            printf("     ");
+            for (i = 0; i < tam; i++)
+            {
+                printf(" %d | ", i+1);
+            }
+            
+            for (i = 0; i < tam; i++)
+            {   
+                
+                printf("\n");
+                printf("%d -", i+1);
+                for (j = 0; j < tam; j++)
+                {   
+                    printf("   %d ", matriz[i][j]);
+                }
+            }
+        }
+
+        printf("\nPara repetir o programa digite 1, para sair 0:");
+        scanf("%d", &repeat);
+    } while (repeat);
+    return 0;
+}
