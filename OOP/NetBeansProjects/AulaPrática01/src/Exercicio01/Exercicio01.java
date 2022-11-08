b7671dbe8d289b4356d14909f81a1ce4cfb505a4
EduardoSchwantz <eduardo_schwantz@hotmail.com>
2022-11-08 09:52:14 -0300

    att java

---
+++ OOP/NetBeansProjects/AulaPrática01/src/Exercicio01/Exercicio01.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exercicio01;
+
+/**
+ *
+ * @author schwantz
+ */
+public class Exercicio01 {
+    public static void main(String[] args) {
+        
+    }
+}
--- OOP/NetBeansProjects/lista1_ex1/src/lista1_ex1/Lista1_ex1.java
+++ OOP/NetBeansProjects/lista1_ex1/src/lista1_ex1/Lista1_ex1.java
@@ -14,104 +14,106 @@ public class Lista1_ex1 {
             System.out.println("Digite 0 para encerrar o programa");
             System.out.println("Digite uma Opção: ");
             option = input.nextInt();
-            switch (option) {
-                case 1:
-                    System.out.println("Imprimindo tabuada do 1:");
-                    x = 0;
-                    for (i = 0; i < 11; i++) {
-                        x = i * 1;
-                        System.out.println(" " + i + " * 1 : " + x + " ");
-                        x++;
+            if (option >= 0 && option < 10) {
+                switch (option) {
+                    case 1:
+                        System.out.println("Imprimindo tabuada do 1:");
+                        x = 0;
+                        for (i = 0; i < 11; i++) {
+                            x = i * 1;
+                            System.out.println(" " + i + " * 1 : " + x + " ");
+                            x++;
+                            break;
+                        }
+                    case 2:
+                        System.out.println("Imprimindo tabuada do 2:");
+                        x = 0;
+                        for (i = 0; i < 11; i++) {
+                            x = i * 2;
+                            System.out.println(" " + i + " * 2 : " + x + " ");
+                            x++;
+                            break;
+                        }
+                    case 3:
+                        System.out.println("Imprimindo tabuada do 3:");
+                        x = 0;
+                        for (i = 0; i < 11; i++) {
+                            x = i * 3;
+                            System.out.println(" " + i + " * 3 : " + x + " ");
+                            x++;
+                            break;
+                        }
+                    case 4:
+                        System.out.println("Imprimindo tabuada do 4:");
+                        x = 0;
+                        for (i = 0; i < 11; i++) {
+                            x = i * 4;
+                            System.out.println(" " + i + " * 4 : " + x + " ");
+                            x++;
+                        }
                         break;
-                    }
-                case 2:
-                    System.out.println("Imprimindo tabuada do 2:");
-                    x = 0;
-                    for (i = 0; i < 11; i++) {
-                        x = i * 2;
-                        System.out.println(" " + i + " * 2 : " + x + " ");
-                        x++;
+                    case 5:
+                        System.out.println("Imprikmindo tabuada do 5:");
+                        x = 0;
+                        for (i = 0; i < 11; i++) {
+                            x = i * 5;
+                            System.out.println(" " + i + " * 5 : " + x + " ");
+                            x++;
+                        }
                         break;
-                    }
-                case 3:
-                    System.out.println("Imprimindo tabuada do 3:");
-                    x = 0;
-                    for (i = 0; i < 11; i++) {
-                        x = i * 3;
-                        System.out.println(" " + i + " * 3 : " + x + " ");
-                        x++;
+                    case 6:
+                        System.out.println("Imprimindo tabuada do 6:");
+                        x = 0;
+                        for (i = 0; i < 11; i++) {
+                            x = i * 6;
+                            System.out.println(" " + i + " * 6 : " + x + " ");
+                            x++;
+                        }
                         break;
-                    }
-                case 4:
-                    System.out.println("Imprimindo tabuada do 4:");
-                    x = 0;
-                    for (i = 0; i < 11; i++) {
-                        x = i * 4;
-                        System.out.println(" " + i + " * 4 : " + x + " ");
-                        x++;
-                    }
-                    break;
-                case 5:
-                    System.out.println("Imprimindo tabuada do 5:");
-                    x = 0;
-                    for (i = 0; i < 11; i++) {
-                        x = i * 5;
-                        System.out.println(" " + i + " * 5 : " + x + " ");
-                        x++;
-                    }
-                    break;
-                case 6:
-                    System.out.println("Imprimindo tabuada do 6:");
-                    x = 0;
-                    for (i = 0; i < 11; i++) {
-                        x = i * 6;
-                        System.out.println(" " + i + " * 6 : " + x + " ");
-                        x++;
-                    }
-                    break;
-                case 7:
-                    System.out.println("Imprimindo tabuada do 7:");
-                    x = 0;
-                    for (i = 0; i < 11; i++) {
-                        x = i * 7;
-                        System.out.println(" " + i + " * 7 : " + x + " ");
-                        x++;
-                    }
-                    break;
-                case 8:
-                    System.out.println("Imprimindo tabuada do 8:");
-                    x = 0;
-                    for (i = 0; i < 11; i++) {
-                        x = i * 8;
-                        System.out.println(" " + i + " * 8 : " + x + " ");
-                        x++;
-                    }
-                    break;
-                case 9:
-                    System.out.println("Imprimindo tabuada do 9:");
-                    x = 0;
-                    for (i = 0; i < 11; i++) {
-                        x = i * 9;
-                        System.out.println(" " + i + " * 9 : " + x + " ");
-                        x++;
-                    }
-                    break;
-                case 10:
-                    System.out.println("Imprimindo tabuada do 10:");
-                    x = 0;
-                    for (i = 0; i < 11; i++) {
-                        x = i * 10;
-                        System.out.println(" " + i + " * 10 : " + x + " ");
-                        x++;
-                    }
-                    break;
-                case 0:
-                    System.out.println("Saindo...");
-                    exit = false;
-                    break;
-                default:
-                    System.out.println("Opção inválida");
-                    break;
+                    case 7:
+                        System.out.println("Imprimindo tabuada do 7:");
+                        x = 0;
+                        for (i = 0; i < 11; i++) {
+                            x = i * 7;
+                            System.out.println(" " + i + " * 7 : " + x + " ");
+                            x++;
+                        }
+                        break;
+                    case 8:
+                        System.out.println("Imprimindo tabuada do 8:");
+                        x = 0;
+                        for (i = 0; i < 11; i++) {
+                            x = i * 8;
+                            System.out.println(" " + i + " * 8 : " + x + " ");
+                            x++;
+                        }
+                        break;
+                    case 9:
+                        System.out.println("Imprimindo tabuada do 9:");
+                        x = 0;
+                        for (i = 0; i < 11; i++) {
+                            x = i * 9;
+                            System.out.println(" " + i + " * 9 : " + x + " ");
+                            x++;
+                        }
+                        break;
+                    case 10:
+                        System.out.println("Imprimindo tabuada do 10:");
+                        x = 0;
+                        for (i = 0; i < 11; i++) {
+                            x = i * 10;
+                            System.out.println(" " + i + " * 10 : " + x + " ");
+                            x++;
+                        }
+                        break;
+                    case 0:
+                        System.out.println("Saindo...");
+                        exit = false;
+                        break;
+                    default:
+                        System.out.println("Opção inválida");
+                        break;
+                }
             }
         }
     }
